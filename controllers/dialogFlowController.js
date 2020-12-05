71e5724448e8639ebb3830c3ed3c1c57dcc53509
jerson <1995.martinez@gmail.com>
2020-12-05 00:36:02 -0300

    feat: se añadio la caracteristica de trasferir dinero

--- controllers/dialogFlowController.js
+++ controllers/dialogFlowController.js
@@ -1,5 +1,5 @@
 const {getConnection} = require('../store/db')
-const {generateCardNumber,formatter} = require('../utils/')
+const {generateCardNumber, formatter} = require('../utils/')
 const {v4} = require('uuid')
 const axios = require('axios');
 const actions = {
@@ -34,18 +34,17 @@ const actions = {
         const db = getConnection();
         const user = db.get('users').find({name: username}).value()
         return {
-            fulfillmentText: `${user.name}! tu saldo actual es de ${formatter.format(user.balance) }`, source: 'session'
+            fulfillmentText: `${user.name}! tu saldo actual es de ${formatter.format(user.balance)}`, source: 'session'
         }
     },
-    INDICATORS:async ({queryResult}) =>{
+    INDICATORS: async ({queryResult}) => {
         try {
             const data = ['valor en CLP']
             const {parameters} = queryResult;
-            if(parameters && parameters.indicator){
+            if (parameters && parameters.indicator) {
                 const resp = await axios.get(`https://mindicador.cl/api/${parameters.indicator}`);
                 data.push(`${resp.data.nombre}: $${resp.data.serie[0].valor}`);
-            }
-            else {
+            } else {
                 const resp = await axios.get("https://mindicador.cl/api");
 
                 for (const key in resp.data) {
@@ -61,36 +60,62 @@ const actions = {
                         text: {
                             text: data
                         }
-                    }], source: 'session'
+                    }], source: 'indicators'
             };
 
-        }catch (e) {
-            console.log(e);
+        } catch (e) {
             return {
-                fulfillmentText: `los datos economicos no estan disponibles en este momento`, source: 'session'
+                fulfillmentText: `los datos economicos no estan disponibles en estos momento`, source: 'indicators'
             }
+        }
+    },
+    TRANSFER: ({queryResult}) => {
+        const {parameters} = queryResult;
+        const username = queryResult.outputContexts[0].parameters.name;
+        if (username && parameters && parameters.account && parameters.amount) {
+            const db = getConnection();
+            const amount =  parseInt(parameters.amount);
+            const user = db.get('users').find({name: username}).value();
+            const userDest = db.get('users').find({account_number: parameters.account}).value();
+            if (user.balance < parseInt(parameters.amount)) {
+                return {
+                    fulfillmentText: `Su cuenta bancario no cuenta con el saldo suficiente para realizar esta operación`,
+                    source: 'session'
+                }
+            }
+            if (userDest) {
+                db.get('users')
+                    .find({id: userDest.id})
+                    .assign({balance: userDest.balance + amount})
+                    .write();
+            }
+            db.get('users')
+                .find({id: user.id})
+                .assign({balance: user.balance - amount})
+                .write();
+            const transfer = {
+                id: v4(),
+                amount: amount,
+                date: new Date(),
+                user_id:user.id,
+                target_account: parameters.account,
+                origin_account: user.account_number
+            }
+            db.get('transfer').push(transfer).write();
+            return {
+                fulfillmentMessages: [
+                    {
+                        text: {
+                            text: [
+                                `Estimado ${user.name} Se realizo con éxito la transferencia a la cuenta bancaria n°${parameters.account}`+
+                                ` por el monto de ${formatter.format(amount)}`,
+                                "En que mas te puedo ayudar ?"
+                            ]
+                        }
+                    }], source: 'session'
+            }
+
         }
     }
 }
 module.exports = actions;
-
-/*
-ref.on("value",(snapshot) => {
-                    const users = snapshot.val()
-                    console.log(users);
-                    //res.json({fulfillmentText: 'hola que tal el dia ' + user.name + '?', source: 'session'});
-                    Object.entries(snapshot).map(([k,v]) => v)
-                    res.json(users);
-                },(errorObject) => {
-                    console.log("The read failed: " + errorObject.code);
-                    const newUser = {
-                        name: username,
-                        phone:" +56972636554",
-                        account_number:generateCardNumber(),
-                        balance:500000
-                    }
-                    ref.push(newUser);
-                    res.json({fulfillmentText: 'hola que tal el dia ' + newUser.name + '?', source: 'session'});
-                })
-
- */
