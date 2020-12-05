3e8c4708e28177244d9e93711859045c4af3f772
jerson <1995.martinez@gmail.com>
2020-12-05 03:55:53 -0300

    refactor: mejoras en las respuestas

--- controllers/dialogFlowController.js
+++ controllers/dialogFlowController.js
@@ -26,7 +26,31 @@ const actions = {
                             `En que puedo ayudarte ${user.name}?`
                         ]
                     }
-                }], source: 'session'
+                },
+                {
+                    payload: {
+                        richContent: [
+                            [
+                                {
+                                    type: "chips",
+                                    options: [
+                                        {
+                                            text: "Cual es mi saldo?"
+                                        },
+                                        {
+                                            text: "Realizar trasferencia "
+                                        },
+                                        {
+                                            text: "Indicadores económicos"
+                                        }
+                                    ]
+                                }
+                            ]
+                        ]
+
+                    }
+                }
+            ], source: 'session'
         }
     },
     BALANCE: ({queryResult}) => {
@@ -35,8 +59,37 @@ const actions = {
         const db = getConnection();
         const user = db.get('users').find({name: username}).value()
         return {
-            fulfillmentText: `${user.name}! tu saldo actual es de ${formatter.format(user.balance)}`, source: 'session'
+            fulfillmentMessages: [
+                {
+                    text: {
+                        text: [
+                            `${user.name}! tu saldo actual es de ${formatter.format(user.balance)}`
+                        ]
+                    }
+                },
+                {
+                    payload: {
+                        richContent: [
+                            [
+                                {
+                                    type: "chips",
+                                    options: [
+                                        {
+                                            text: "Indicadores económicos"
+                                        },
+                                        {
+                                            text: "Realizar trasferencia "
+                                        }
+                                    ]
+                                }
+                            ]
+                        ]
+
+                    }
+                }
+            ], source: 'session'
         }
+
     },
     INDICATORS: async ({queryResult}) => {
         try {
@@ -55,6 +108,42 @@ const actions = {
                     }
                 }
             }
+            const content = [
+                {
+                    type: "description",
+                    title: "Indicadores económicos (valoración en CLP)",
+                    text: data
+                }
+            ];
+            if(
+                queryResult.outputContexts
+                &&
+                queryResult.outputContexts[0].parameters &&
+                queryResult.outputContexts[0].parameters.name
+            ){
+                content.push({
+                    type: "chips",
+                    options: [
+                        {
+                            text: "Cual es mi saldo?"
+                        },
+                        {
+                            text: "Realizar trasferencia "
+                        }
+                    ]
+                });
+            }else
+                content.push({
+                    type: "chips",
+                    options: [
+                        {
+                            text: "Quiero iniciar sesion"
+                        },
+                        {
+                            text: "Indicadores económicos"
+                        }
+                    ]
+                })
             return {
                 fulfillmentMessages: [
                     {
@@ -67,13 +156,7 @@ const actions = {
                     {
                         payload: {
                             richContent: [
-                                [
-                                    {
-                                        type: "description",
-                                        title: "Valoración en CLP",
-                                        text: data
-                                    }
-                                ]
+                                content
                             ]
 
                         }
@@ -148,6 +231,17 @@ const actions = {
                                             }
                                         },
                                         actionLink: `${url}/api/proof/?doc=${proofName}`
+                                    },
+                                    {
+                                        type: "chips",
+                                        options: [
+                                            {
+                                                text: "Indicadores económicos"
+                                            },
+                                            {
+                                                text: "Cual es mi saldo?"
+                                            }
+                                        ]
                                     }
                                 ]
                             ]
